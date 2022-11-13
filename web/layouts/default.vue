a62e6dd9c8523fdf229b85661b6df7a3114fb180
inkfarer <inkfarer@inkfarer.com>
2022-11-13 12:00:32 +0200

    Fix a component's styles unexpectedly modifying the style of every page

---
+++ web/layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <slot />
+    </div>
+</template>
+
+<style lang="scss">
+body {
+    margin: 0;
+    min-height: 100vh;
+    min-width: 100%;
+}
+</style>
--- web/pages/play.vue
+++ web/pages/play.vue
@@ -5,12 +5,6 @@
 </template>
 
 <style lang="scss">
-body {
-    margin: 0;
-    min-height: 100vh;
-    min-width: 100%;
-}
-
 .game-wrapper {
     width: 100%;
     height: 100vh;
