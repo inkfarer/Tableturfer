ecb0fa8aabdcafbd32ac18927080faae7bff7d3a
inkfarer <inkfarer@inkfarer.com>
2022-12-01 02:02:31 +0200

    server: Add real card data

--- server/src/game/card.rs
+++ server/src/game/card.rs
@@ -1,22 +1,30 @@
+use std::collections::HashMap;
+use serde::Deserialize;
+use serde_repr::Deserialize_repr;
 use crate::matrix::Matrix;
 
-#[derive(Clone, Copy, Eq, PartialEq, Debug)]
+static CARDS_JSON: &str = include_str!("cards.json");
+
+#[derive(Clone, Copy, Eq, PartialEq, Debug, Deserialize_repr)]
+#[repr(u8)]
 pub enum CardSquareType {
-    Empty,
-    Fill,
-    Special,
+    Empty = 0,
+    Fill = 1,
+    Special = 2,
 }
 
 pub type CST = CardSquareType;
 
+#[derive(Clone, Deserialize_repr)]
+#[repr(u8)]
 pub enum CardRarity {
-    Common,
-    Rare,
-    Fresh,
+    Common = 0,
+    Rare = 1,
+    Fresh = 2,
 }
 
+#[derive(Deserialize, Clone)]
 pub struct Card {
-    pub row_id: String,
     pub category: String,
     pub name: String,
     pub number: usize,
@@ -31,71 +39,25 @@ pub trait CardSquareProvider {
     fn get(&self, card_name: &str) -> Option<Card>;
 }
 
-pub struct CardSquareProviderImpl {}
+pub struct CardSquareProviderImpl {
+    cards: HashMap<String, Card>,
+}
 
 impl CardSquareProviderImpl {
     pub fn new() -> Self {
-        CardSquareProviderImpl {}
+        let card_list: Vec<Card> = serde_json::from_str(CARDS_JSON).unwrap();
+
+        CardSquareProviderImpl {
+            cards: card_list.into_iter()
+                .map(|card| (card.name.clone(), card))
+                .collect()
+        }
     }
 }
 
 impl CardSquareProvider for CardSquareProviderImpl {
-    // todo: doesn't belong here
     fn get(&self, card_name: &str) -> Option<Card> {
-        match card_name {
-            "BombCurling" => Some(Card {
-                row_id: "MiniGame_BombCurling".to_owned(),
-                category: "WeaponSub".to_owned(),
-                name: "BombCurling".to_owned(),
-                number: 62,
-                rarity: CardRarity::Common,
-                season: 1,
-                special_cost: 2,
-                squares: Matrix::new(vec!(
-                    vec!(CST::Empty, CST::Special, CST::Empty),
-                    vec!(CST::Fill, CST::Fill, CST::Fill),
-                )),
-            }),
-            "BombQuick" => Some(Card {
-                row_id: "MiniGame_BombQuick".to_owned(),
-                category: "WeaponSub".to_owned(),
-                name: "BombQuick".to_owned(),
-                number: 58,
-                rarity: CardRarity::Common,
-                season: 1,
-                special_cost: 1,
-                squares: Matrix::new(vec!(
-                    vec!(CST::Special),
-                )),
-            }),
-            "ChargerLight00" => Some(Card {
-                row_id: "MiniGame_ChargerLight00".to_owned(),
-                category: "WeaponMain".to_owned(),
-                name: "ChargerLight00".to_owned(),
-                number: 32,
-                rarity: CardRarity::Common,
-                season: 1,
-                special_cost: 3,
-                squares: Matrix::new(vec!(
-                    vec!(CST::Fill, CST::Empty, CST::Empty, CST::Empty, CST::Fill),
-                    vec!(CST::Fill, CST::Special, CST::Fill, CST::Fill, CST::Fill),
-                )),
-            }),
-            "ChargerNormal00" => Some(Card {
-                row_id: "MiniGame_ChargerNormal00".to_owned(),
-                category: "WeaponMain".to_owned(),
-                name: "ChargerNormal00".to_owned(),
-                number: 28,
-                rarity: CardRarity::Common,
-                season: 1,
-                special_cost: 3,
-                squares: Matrix::new(vec!(
-                    vec!(CST::Fill, CST::Fill, CST::Fill, CST::Fill, CST::Fill, CST::Fill, CST::Fill),
-                    vec!(CST::Empty, CST::Empty, CST::Special, CST::Empty, CST::Empty, CST::Empty, CST::Empty),
-                )),
-            }),
-            _ => None
-        }
+        self.cards.get(card_name).map(|card| card.clone())
     }
 }
 
@@ -116,7 +78,6 @@ pub mod tests {
         fn get(&self, card_name: &str) -> Option<Card> {
             match card_name {
                 "card_1" => Some(Card {
-                    row_id: "row_id_1".to_string(),
                     category: "test_cards".to_string(),
                     name: "card_1".to_string(),
                     number: 1,
@@ -130,7 +91,6 @@ pub mod tests {
                     )),
                 }),
                 "card_2" => Some(Card {
-                    row_id: "row_id_2".to_string(),
                     category: "test_cards".to_string(),
                     name: "card_2".to_string(),
                     number: 2,
@@ -143,7 +103,6 @@ pub mod tests {
                     )),
                 }),
                 "card_3" => Some(Card {
-                    row_id: "row_id_3".to_string(),
                     category: "test_cards".to_string(),
                     name: "card_3".to_string(),
                     number: 3,
@@ -155,7 +114,6 @@ pub mod tests {
                     )),
                 }),
                 "card_4" => Some(Card {
-                    row_id: "row_id_3".to_string(),
                     category: "test_cards".to_string(),
                     name: "card_4".to_string(),
                     number: 4,
