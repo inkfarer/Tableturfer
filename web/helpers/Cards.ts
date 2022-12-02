f412c2cd407d2ab104d4e2fb1fd3ca459a4302bf
inkfarer <inkfarer@inkfarer.com>
2022-12-02 06:13:16 +0200

    client: Attempt to prevent card data from being incidentally modified

--- web/helpers/Cards.ts
+++ web/helpers/Cards.ts
@@ -5,5 +5,5 @@ export const CardMap: Map<string, Card> = new Map();
 
 for (let i = 0; i < Cards.length; i++) {
     const card = Cards[i];
-    CardMap.set(card.name, card);
+    CardMap.set(card.name, Object.freeze(card));
 }
