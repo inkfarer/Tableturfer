92da031acf9a54ce8ed7a93403022989477ccb6a
inkfarer <inkfarer@inkfarer.com>
2022-12-06 03:50:41 +0200

    parser: Add lockfile for msbt library

--- web/nuxt.config.ts
+++ web/nuxt.config.ts
@@ -1,13 +1,35 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+    css: [
+        '@/assets/style-common.scss',
+        '/node_modules/normalize.css/normalize.css'
+    ],
     imports: {
         autoImport: false
     },
     modules: [
-        '@pinia/nuxt'
+        '@pinia/nuxt',
+        '~/modules/CardI18nModule.ts',
+        '@nuxtjs/i18n'
     ],
     typescript: {
         typeCheck: true
+    },
+    i18n: {
+        locales: [
+            {
+                code: 'en',
+                file: 'en.json'
+            }
+        ],
+        lazy: true,
+        langDir: 'lang',
+        defaultLocale: 'en',
+        strategy: 'no_prefix',
+        vueI18n: {
+            legacy: false,
+            locale: 'en'
+        }
     },
     runtimeConfig: {
         public: {
