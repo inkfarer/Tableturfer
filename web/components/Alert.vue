748096167653beb2247d3f5ca745d9ce675e0cc0
inkfarer <inkfarer@inkfarer.com>
2022-12-13 05:45:59 +0200

    client: Add more style options to Alert component

--- web/components/Alert.vue
+++ web/components/Alert.vue
@@ -1,14 +1,35 @@
 <template>
-    <div class="alert">
+    <div
+        class="alert"
+        :class="`theme-${props.theme}`"
+    >
         <slot />
     </div>
 </template>
 
+<script lang="ts" setup>
+const props = withDefaults(defineProps<{
+    theme?: 'error' | 'info'
+}>(), {
+    theme: 'error'
+});
+</script>
+
 <style lang="scss" scoped>
 .alert {
-    padding: 8px;
+    padding: 8px 12px;
     border-radius: 16px;
-    border: 2px solid $error-red;
-    background-color: $error-red-a20;
+    border-style: solid;
+    border-width: 2px;
+
+    &.theme-error {
+        border-color: $error-red;
+        background-color: $error-red-a20;
+    }
+
+    &.theme-info {
+        border-color: $info-blue;
+        background-color: $info-blue-a20;
+    }
 }
 </style>
