f1cfb1b224dc5ee756113d2757d2eff49701184d
inkfarer <inkfarer@inkfarer.com>
2023-01-08 12:56:19 +0200

    client: Fix single taps on a touchscreen moving the selected card around the game board

--- web/composables/UseSwipeCardMovement.ts
+++ web/composables/UseSwipeCardMovement.ts
@@ -1,4 +1,4 @@
-import { computed, ref, useElementSize, useMouseInElement, useSwipe, watch } from '#imports';
+import { computed, ref, useElementBounding, usePointer, useSwipe, watch } from '#imports';
 import { useGameBoardStore } from '~/stores/GameBoardStore';
 import { useCurrentMoveStore } from '~/stores/CurrentMoveStore';
 import { VueInstance } from '@vueuse/core';
@@ -9,7 +9,7 @@ export default function (target: Ref<VueInstance | null>) {
     const activeCardStore = useCurrentMoveStore();
 
     const boardSize = computed(() => gameBoardStore.boardSize);
-    const boardElementSize = useElementSize(target);
+    const boardElementSize = useElementBounding(target);
     const pxToMoveOneSquare = computed(() => Math.min(boardElementSize.width.value, boardElementSize.height.value) / Math.max(boardSize.value.width, boardSize.value.height));
 
     const amountMoved = ref<{ x: number | null, y: number | null }>({ x: null, y: null });
@@ -32,21 +32,19 @@ export default function (target: Ref<VueInstance | null>) {
         }
     });
 
-    const mouseInGameBoard = useMouseInElement(target);
+    const mouseInGameBoard = usePointer({ target: target as Ref<EventTarget | null>, pointerTypes: ['mouse']});
     const squareSizePx = computed(() => Math.min(
-        mouseInGameBoard.elementHeight.value / boardSize.value.height,
-        mouseInGameBoard.elementWidth.value / boardSize.value.width));
+        boardElementSize.height.value / boardSize.value.height,
+        boardElementSize.width.value / boardSize.value.width));
     const boardMargin = computed(() => ({
-        x: (mouseInGameBoard.elementWidth.value - (squareSizePx.value * boardSize.value.width)) / 2,
-        y: (mouseInGameBoard.elementHeight.value - (squareSizePx.value * boardSize.value.height)) / 2
+        x: (boardElementSize.width.value - (squareSizePx.value * boardSize.value.width)) / 2,
+        y: (boardElementSize.height.value - (squareSizePx.value * boardSize.value.height)) / 2
     }));
 
-    watch(() => ({ x: mouseInGameBoard.elementX.value, y: mouseInGameBoard.elementY.value }), newValue => {
-        if (mouseInGameBoard.sourceType.value === 'mouse') {
-            activeCardStore.setPositionIfPossible({
-                x: Math.floor((newValue.x - boardMargin.value.x) / squareSizePx.value),
-                y: Math.floor((newValue.y - boardMargin.value.y) / squareSizePx.value)
-            }, true);
-        }
+    watch(() => ({ x: mouseInGameBoard.x.value, y: mouseInGameBoard.y.value }), newValue => {
+        activeCardStore.setPositionIfPossible({
+            x: Math.floor((newValue.x - boardElementSize.left.value - boardMargin.value.x) / squareSizePx.value),
+            y: Math.floor((newValue.y - boardElementSize.top.value - boardMargin.value.y) / squareSizePx.value)
+        }, true);
     });
 }
