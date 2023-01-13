d791958611263e84493cc13a5e1efc9b7ecfcb61
inkfarer <inkfarer@inkfarer.com>
2023-01-13 20:52:47 +0200

    client: Hide movement keys when instructed

--- web/components/game/GameCardActionKeys.vue
+++ web/components/game/GameCardActionKeys.vue
@@ -1,36 +1,55 @@
 <template>
     <div class="card-movement-keys">
-        <div class="keypad">
+        <template v-if="userSettingsStore.useOnScreenControls">
+            <div class="keypad">
+                <GameCardActionKey
+                    icon="fa6-solid:arrow-rotate-left"
+                    @click="currentMoveStore.previousRotationStep()"
+                />
+                <GameCardActionKey
+                    icon="fa6-solid:angle-up"
+                    @click="currentMoveStore.moveUp()"
+                />
+                <GameCardActionKey
+                    icon="fa6-solid:arrow-rotate-right"
+                    @click="currentMoveStore.nextRotationStep()"
+                />
+                <GameCardActionKey
+                    icon="fa6-solid:angle-left"
+                    @click="currentMoveStore.moveLeft()"
+                />
+                <GameCardActionKey
+                    icon="fa6-solid:angle-down"
+                    @click="currentMoveStore.moveDown()"
+                />
+                <GameCardActionKey
+                    icon="fa6-solid:angle-right"
+                    @click="currentMoveStore.moveRight()"
+                />
+            </div>
+
+            <div class="extra-buttons">
+                <GameCardActionKey
+                    :text="$t('game.placeCard')"
+                    @click="placeCard"
+                />
+            </div>
+        </template>
+        <div
+            v-else
+            class="keypad without-movement-keys"
+        >
             <GameCardActionKey
                 icon="fa6-solid:arrow-rotate-left"
                 @click="currentMoveStore.previousRotationStep()"
             />
             <GameCardActionKey
-                icon="fa6-solid:angle-up"
-                @click="currentMoveStore.moveUp()"
+                :text="$t('game.placeCard')"
+                @click="placeCard"
             />
             <GameCardActionKey
                 icon="fa6-solid:arrow-rotate-right"
                 @click="currentMoveStore.nextRotationStep()"
-            />
-            <GameCardActionKey
-                icon="fa6-solid:angle-left"
-                @click="currentMoveStore.moveLeft()"
-            />
-            <GameCardActionKey
-                icon="fa6-solid:angle-down"
-                @click="currentMoveStore.moveDown()"
-            />
-            <GameCardActionKey
-                icon="fa6-solid:angle-right"
-                @click="currentMoveStore.moveRight()"
-            />
-        </div>
-
-        <div class="extra-buttons">
-            <GameCardActionKey
-                :text="$t('game.placeCard')"
-                @click="placeCard"
             />
         </div>
     </div>
@@ -38,8 +57,10 @@
 
 <script lang="ts" setup>
 import { useCurrentMoveStore } from '~/stores/CurrentMoveStore';
+import { useUserSettingsStore } from '~/stores/UserSettingsStore';
 
 const currentMoveStore = useCurrentMoveStore();
+const userSettingsStore = useUserSettingsStore();
 
 function placeCard() {
     currentMoveStore.proposeMove();
@@ -56,6 +77,13 @@ function placeCard() {
         display: grid;
         grid-template-columns: repeat(3, 1fr);
         gap: 4px;
+
+        &.without-movement-keys {
+            width: 100%;
+            max-width: 400px;
+            display: flex;
+            justify-content: space-between;
+        }
     }
 
     .extra-buttons {
@@ -64,6 +92,14 @@ function placeCard() {
         grid-template-columns: 1fr;
         gap: 4px;
         justify-items: center;
+    }
+}
+
+@include media-breakpoint-down(sm) {
+    .keypad > * {
+        font-size: 1em !important;
+        line-height: 1.1;
+        padding: 8px 10px !important;
     }
 }
 </style>
