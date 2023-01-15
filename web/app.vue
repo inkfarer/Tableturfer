6bf2549128b599f516ec11d08223eb3e1596b03f
inkfarer <inkfarer@inkfarer.com>
2023-01-15 18:42:06 +0200

    client: Add a default page title

---
+++ web/app.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <NuxtLayout>
+            <NuxtPage />
+        </NuxtLayout>
+    </div>
+</template>
+
+<script lang="ts" setup>
+import { useHead, useI18n } from '#imports';
+
+const i18n = useI18n();
+
+useHead({
+    titleTemplate: chunk => chunk == null ? i18n.t('title.default') : i18n.t('title.template', { page: chunk })
+});
+</script>
